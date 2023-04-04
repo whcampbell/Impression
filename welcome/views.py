a68c2d5d39aaebb1513f27750a35bd3f19fe6f7e
Will <Campbell>
2023-04-04 16:54:15 -0500

    initial commit

---
+++ welcome/views.py
@@ -0,0 +1,9 @@
+from django.template import loader
+from django.http import HttpResponse
+
+def index(request) :
+    template = loader.get_template('welcome/index.html')
+    return HttpResponse(template.render())
+
+
+# Create your views here.
