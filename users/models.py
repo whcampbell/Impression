e76fc688df4cb90c6b387b4d1f39bf8e1522f526
Will <whcampbell@wisc.edu>
2023-04-18 00:24:44 -0500

    Created User Tests  - checked username, password, authentication  - changed user model to use django current time as default

--- users/models.py
+++ users/models.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from datetime import date
+from django.utils import timezone
 
 class CustomUser(AbstractUser) :
 
-    artist_since = models.DateField(default=date.today())
+    artist_since = models.DateField(default=timezone.now)
 
     def __str__(self) :
         return self.username
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from .models import CustomUser
+
+class UserTestCase(TestCase) :
+    def setUp(self) :
+        user = "TEST"
+        email = "test@gmail.com"
+        since = "2000-09-20"
+        testuser = CustomUser.objects.create(
+            username=user, 
+            email=email,
+            artist_since=since,
+            )
+        testuser.set_password("thisisatest00")
+        testuser.save()
+    
+    def test_user_created(self) :
+        testuser = CustomUser.objects.get(username="TEST")
+        self.assertEqual(testuser.username, "TEST")
+
+    def test_user_authentication(self) :
+        testuser = CustomUser.objects.get(username="TEST")
+        self.assertTrue(testuser.is_authenticated)
+
+    def test_check_password(self) :
+        testuser = CustomUser.objects.get(username="TEST")
+        #testuser.set_password("thisisatest00")
+        self.assertTrue(testuser.check_password("thisisatest00"))
+
+
 
 # Create your tests here.
