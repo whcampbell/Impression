4b5e0879d955039cdf1b6c8abe7ccf39c6cdc494
Will <whcampbell@wisc.edu>
2023-04-19 23:53:14 -0500

    Connected Base html  - Added project-wide template dir  - adapted welcome page to extend base

--- Impression/settings.py
+++ Impression/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'Impression.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'Impression/templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
